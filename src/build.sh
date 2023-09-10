e43aa69ef48b998375cb78b73e5026d740f5425b
Dmytro Melnyk <damelnik87@gmail.com>
2023-09-10 22:47:25 +0300

    fix: endings1

--- src/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OUT_DIR="asset-output" # the path is expected as a result of bundler's work
-
-EXTRA_REQ=$(if [ -f "${LAMBDA_ROOT_PATH}/requirements.txt" ]; then echo "-r ${LAMBDA_ROOT_PATH}/requirements.txt"; fi)
-pip install -r common/requirements.txt $EXTRA_REQ -t /$OUT_DIR
-
-cp -au common /$OUT_DIR/common
-cp -au $LAMBDA_ROOT_PATH /$OUT_DIR/$LAMBDA_ROOT_PATH
--- src/hello/common.py
+++
@@ -1,8 +0,0 @@
-from functools import cache
-
-import boto3
-
-
-@cache
-def dynamodb_resource():
-    return boto3.resource(service_name="dynamodb")
