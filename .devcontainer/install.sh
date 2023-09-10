ae7df99d792ec2901f00db70eea7737d550c26d6
Dmytro Melnyk <damelnik87@gmail.com>
2023-09-10 18:46:36 +0000

    fix: small fixes for win os

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -3,6 +3,8 @@ complete -C '/usr/local/bin/aws_completer' aws
 complete -C '/usr/local/bin/aws_completer' awslocal
 EOF
 
+# fix ownership for windows' mounted volume
+sudo chown -R vscode:vscode .git
 pip install --user -r requirements.txt
 pip install --user -r requirements-dev.txt
 pip install --user -r src/common/requirements.txt
